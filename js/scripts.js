d6f768752e5ca84d1843a730a262d0b0234c1b92
Hristo Dzhambov and Nick Gallo <nick@>
2020-01-23 16:30:48 -0800

    finalize functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-$(document).ready(function(event){
- 
-  })
-
+$(document).ready(function (event) {
+  $("form#word-form").submit(function (event) {
+    event.preventDefault();
+    var result = [];
+    var sentence = $("input#word").val().split('');
+    for (i = 0; i <= sentence.length; i++) {
+      var vowels = ["A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y"];
+      if (vowels.includes(sentence[i])) {
+        result.push("-");
+      } else {
+        result.push(sentence[i]);
+      }
+    }
+    $("#output").append(result);
+    // console.log(userInput);
+  });
+});
 
 
